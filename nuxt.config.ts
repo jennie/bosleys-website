6d6f1c73af371edd34c5b6382d05847d9823b4e8
Jennie Robinson Faber <jennie@jenniefaber.com>
2025-03-31 10:49:22 -0400

    Update Netlify configuration to change build command and output directory. Enhance Nuxt configuration by adding CORS headers for API routes.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,9 +7,14 @@ export default defineNuxtConfig({
 
   // Add nitro configuration for file uploads
   nitro: {
-    preset: 'netlify',
-    prerender: {
-      routes: ['/']
+    routeRules: {
+      '/api/**': {
+        cors: true,
+        headers: {
+          'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+          'Access-Control-Allow-Origin': '*'
+        }
+      }
     }
   },
 
